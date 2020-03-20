62bce20784df853794dc598ecef6998dd8e4b1aa
coinevo <coinevo12@gmail.com>
2020-03-20 08:44:33 +0100

    move version to 2.0.1-evo

--- external-libs/script/coinevo-fetch.sh
+++ external-libs/script/coinevo-fetch.sh
@@ -7,7 +7,7 @@ source script/env.sh
 cd $EXTERNAL_LIBS_BUILD_ROOT
 
 url="https://github.com/coinevo/coinevo"
-version="v1.0.1-evo"
+version="2.0.1-evo"
 
 if [ ! -d "coinevo" ]; then
   git clone ${url} -b ${version}
